c08093fef92f384eba0cd5d5db6b2add2267bae6
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-07-17 21:42:39 -0700

    added uuid for userid, added userid in cart and user tables

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const app = express();
+const { v4: uuidv4 } = require('uuid');//generate random id
 
 require('dotenv').config();
 
@@ -43,14 +44,21 @@ app.use(async function mysqlConnection(req, res, next) {
       throw err;
     }
   });
-  
+
 
 app.post('/register', async (req, res) => {
+  
 
   const checkEmail = await req.db.query(
     `SELECT email FROM yugioh_price_checker_users  
     WHERE email = :email`,
     { email: req.body.email });
+
+    let userIdInfo = await req.db.query(
+      `SELECT userId FROM yugioh_price_checker_users`
+      )
+      console.log(userIdInfo)
+      
 
     let hashedPassword = await bcrypt.hash(req.body.password, salt);
 
@@ -58,31 +66,49 @@ app.post('/register', async (req, res) => {
     email:${req.body.email} 
     username:${req.body.username} 
     password:${req.body.password}
-    hashed pass: ${hashedPassword}`)
+    hashed pass: ${hashedPassword}
+    userID:${uuidv4()}`)
 
   try {
   if(checkEmail[0][0] != undefined){
-    return console.log("email is already in use")
+    return console.error("email is already in use")
   }else{
+  
+
     const registration = await req.db.query(
       `INSERT INTO yugioh_price_checker_users
-      (email, username, password)
-      VALUES( :email, :username, :password)`,
+      (email, username, password, userId)
+      VALUES( :email, :username, :password, :userId)`,
       {
         email: req.body.email,
         username: req.body.username,
-        password: hashedPassword
+        password: hashedPassword,
+        userId: uuidv4()
       }
     )
       console.log("successfully registered new user")   
   };
-
     
   }catch(err){
-    console.log('post err card not added', err);
     res.status(500).json({ error: 'Failed to register' });
   }
 });
+
+
+app.get('/checkUserId', async (req, res) => {
+   try{
+        const getUserId = await req.db.query(`
+        SELECT userId, email FROM yugioh_price_checker_users`)
+        
+          return res.json(getUserId)
+        
+  }
+  catch (error) {
+        console.error('Error while querying userId:', error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+  } 
+})
+ 
 
 function verifyJwt(req, res, next){
     const token = req.headers["access-token"];
@@ -106,7 +132,7 @@ app.get('/checkAuth', verifyJwt, (req,res) => {
 
 app.post('/login', async (req, res) => {
   const userInfo = await req.db.query(
-    `SELECT id ,password FROM yugioh_price_checker_users 
+    `SELECT id, email, userId, password FROM yugioh_price_checker_users 
     WHERE username = :username `, 
     {username: req.body.username}
     );
@@ -115,10 +141,12 @@ app.post('/login', async (req, res) => {
 
   if(matchPassword){ 
     //set jwt key here
+    const email = userInfo[0][0].email;
     const id = userInfo[0][0].id;
+    const userId = userInfo[0][0].userId;
     const token = jwt.sign({id}, "jwtsecretkey", {expiresIn: 300})
     console.log("login successful")
-    return res.json({Login:true, "accessToken":token})
+    return res.json({Login:true, "accessToken":token, "email":email, "userId":userId})
   }else{
     console.log("incorrect password")
   }
@@ -133,27 +161,29 @@ app.post('/login', async (req, res) => {
 
 app.get('/cart/list', async (req, res) => {
   //this is the data that comes from react when clicking on the + button
-  const cartList = await req.db.query(`SELECT * FROM yugioh_cart_list WHERE userId = :userId`,
-  {
-    userId: req.body.userId
-  });
+  const cartList = await req.db.query(`SELECT * FROM yugioh_cart_list`);
   res.json(cartList);
 }); 
+
 
 //COMPLETED
 //2. when you click a button, then it will send a post request to the sql server
 //this function adds quantity if the card exists
 app.put('/cart/add', async (req, res) => {
+  const userIdFromClientSide = req.body.userId;
+  console.log("USERID:", userIdFromClientSide)
   try {
     // Check if card already exists in cart list
     const existingCard = await req.db.query(
-      `SELECT quantity FROM yugioh_cart_list WHERE card_name = :card_name AND cartId = :cartId AND userId = :userId`,
+      `SELECT quantity FROM yugioh_cart_list 
+      WHERE card_name = :card_name AND cartId = :cartId AND userId = :userId`,
       {
         card_name: req.body.card_name,
         cartId: req.body.cartId,
-        userId: req.body.userId
+        userId: userIdFromClientSide
       }
     );
+    console.log("TRYING TO ADD CARD TO CART", req.body.card_name, req.body.userId);
 
     if (existingCard[0][0] != undefined) {
       // If card already exists, update its quantity
@@ -166,7 +196,7 @@ app.put('/cart/add', async (req, res) => {
           card_name: req.body.card_name,
           cartId: req.body.cartId,
           price: req.body.price,
-          userId: req.body.userId
+          userId: userIdFromClientSide
         }
       );
 
@@ -193,7 +223,7 @@ app.put('/cart/add', async (req, res) => {
           price: req.body.price,
           quantity: req.body.quantity,
           cartId: req.body.cartId,
-          userId: req.body.userId
+          userId: userIdFromClientSide
         }
       );
 
@@ -211,11 +241,12 @@ app.put('/cart/add', async (req, res) => {
 //function to subtract 1 and delete when quantity is 0 
 app.put('/cart/updateSubtractItem', async(req, res, next) => {
   //if quantity is 0 then delete
+  const userIdFromClientSide = req.body.userId;
   const selectedCard = await req.db.query(
     `SELECT id, quantity FROM yugioh_cart_list WHERE cartId = :cartId AND userId = :userId`,
     { 
       cartId: req.body.cartId,
-      userId:req.body.userId 
+      userId: userIdFromClientSide 
      });
   
    try{
@@ -231,7 +262,7 @@ app.put('/cart/updateSubtractItem', async(req, res, next) => {
         WHERE id = :id AND userId = :userId`,
         {
           id: selectedCard[0][0].id,
-          userId: req.body.userId 
+          userId: userIdFromClientSide 
         },
         (error, result) => {
           console.log('ERRaaOR DELETING IN PUT REQ', error);
@@ -248,7 +279,7 @@ app.put('/cart/updateSubtractItem', async(req, res, next) => {
        WHERE id = :id AND userId = :userId`, 
     {
       id: selectedCard[0][0].id,
-      userId: req.body.userId 
+      userId: userIdFromClientSide 
     }, (error, result) => {
         console.log('ERROR IN SUBTRACTING 1 QUANTITY', error);
         console.log('SUCCESS IN SUBTRACTING 1 QUANTITY', result);
@@ -262,6 +293,7 @@ app.put('/cart/updateSubtractItem', async(req, res, next) => {
 })
 
 app.delete('/cart/deleteItem', async (req, res) => {
+  const userIdFromClientSide = req.body.userId;
     //delete selected row
     //obtain id using name
     //get index of name and then get id from that 
@@ -271,7 +303,7 @@ app.delete('/cart/deleteItem', async (req, res) => {
       {
         card_name: req.body.card_name,
         cartId: req.body.cartId,
-        userId: req.body.userId 
+        userId: userIdFromClientSide 
       }
     );
     console.log("deleting this one",existingCard[0])
@@ -282,7 +314,7 @@ app.delete('/cart/deleteItem', async (req, res) => {
           WHERE id = :id AND userId = :userId`,
           {
             id: existingCard[0][0].id,
-            userId: req.body.userId 
+            userId: userIdFromClientSide 
           }
         );
         
