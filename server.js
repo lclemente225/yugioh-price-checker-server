cc8671daeeb4efacd99f0bfe6fb31ffe2f4d7aac
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-09-15 20:00:22 -0700

    added another link to the cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ require('dotenv').config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const allowedOrigins = ['https://main--ygo-pricechecker.netlify.app', 'http://localhost:5173/'];
+const allowedOrigins = ['https://main--ygo-pricechecker.netlify.app', 'http://localhost:5173/', 'https://ygo-pricechecker.netlify.app/'];
 const corsOptions = {
   origin: function (origin, callback) {
     if (allowedOrigins.includes(origin) || !origin) {
